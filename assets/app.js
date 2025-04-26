e61a49fbca3e4e5b45fa6b00c4624651ff48bf3b
orinaya <orianefrn@gmail.com>
2025-04-27 00:46:22 +0200

    feat(nav): add sidebar with navigation

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,12 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
+window.toggleAccordion = function(id) {
+    const submenu = document.getElementById(id);
+    submenu.classList.toggle('hidden');
+
+    const button = document.querySelector(`button[onclick="toggleAccordion('${id}')"]`);
+    button.classList.toggle('active');
+}
